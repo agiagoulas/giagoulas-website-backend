967de7c2f4a9a682390579f414d2755ef876f691
Alexander Giagoulas <alexander@giagoulas.com>
2023-01-22 15:39:34 +0100

    WIP

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 from lagom import Container
 from lagom.integrations.fast_api import FastApiIntegration
 
-from internal.abstractions.database import Database
-from internal.abstractions.object_storage import ObjectStore
-from internal.repositories.mongo_database import MongoDatabase
-from internal.repositories.s3_bucket import S3Bucket
+from app.internal.abstractions.database import Database
+from app.internal.abstractions.object_storage import ObjectStore
+from app.internal.repositories.mongo_database import MongoDatabase
+from app.internal.repositories.s3_bucket import S3Bucket
 
 container = Container()
 container[Database] = MongoDatabase
