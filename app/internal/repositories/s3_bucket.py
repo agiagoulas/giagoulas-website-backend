fdf92024b0e1d6386ced2f860465467b05a3ba3c
Alexander Giagoulas <alexander@giagoulas.com>
2023-01-22 15:52:09 +0100

    rename env

--- app/internal/repositories/s3_bucket.py
+++ app/internal/repositories/s3_bucket.py
@@ -3,14 +3,14 @@ import boto3
 from uuid import uuid4
 from typing import BinaryIO
 
-AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
 AWS_IMAGE_BUCKET_NAME = os.getenv("AWS_IMAGE_BUCKET")
 
 s3 = boto3.client(
     's3',
-    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     region_name=AWS_DEFAULT_REGION)
 
