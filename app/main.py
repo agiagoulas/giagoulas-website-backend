ae9cd25a7a2f2cb6693f9aadbdec5b62710d68e0
Alexander Giagoulas <alexander@giagoulas.com>
2023-01-22 16:13:04 +0100

    add hello world test

--- app/main.py
+++ app/main.py
@@ -14,6 +14,12 @@ app = FastAPI(title="Giagoulas Website Backend")
 app.include_router(galleries_router)
 app.include_router(posts_router)
 
+
+@app.get("/")
+async def test():
+    return "Hello World"
+
+
 handler = Mangum(app=app, lifespan="off")
 
 if __name__ == "__main__":
