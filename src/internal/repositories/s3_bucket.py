c7e7cbc579ce4197a921a0c9493f3f6fa0c58d1d
Alexander Giagoulas <alexander@giagoulas.com>
2023-01-22 14:16:00 +0100

    rename aws region variable

--- src/internal/repositories/s3_bucket.py
+++ src/internal/repositories/s3_bucket.py
@@ -5,14 +5,14 @@ from typing import BinaryIO
 
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_S3_REGION = os.getenv("AWS_S3_REGION")
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
 AWS_IMAGE_BUCKET_NAME = os.getenv("AWS_IMAGE_BUCKET")
 
 s3 = boto3.client(
     's3',
     aws_access_key_id=AWS_ACCESS_KEY,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_S3_REGION)
+    region_name=AWS_DEFAULT_REGION)
 
 
 class S3Bucket:
@@ -23,7 +23,7 @@ class S3Bucket:
         image_key = f"{uuid4()}-{filename}"
         self.client.upload_fileobj(Fileobj=file, Bucket=AWS_IMAGE_BUCKET_NAME, Key=image_key)
         self.client.put_object_acl(ACL="public-read", Bucket=AWS_IMAGE_BUCKET_NAME, Key=image_key)
-        return f"https://{AWS_IMAGE_BUCKET_NAME}.s3.{AWS_S3_REGION}.amazonaws.com/{image_key}", image_key
+        return f"https://{AWS_IMAGE_BUCKET_NAME}.s3.{AWS_DEFAULT_REGION}.amazonaws.com/{image_key}", image_key
 
     def delete_image(self, image_key: str):
         self.client.delete_object(Bucket=AWS_IMAGE_BUCKET_NAME, Key=image_key)
