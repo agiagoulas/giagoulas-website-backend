0d8a7a2907eb138c4d87e3c4aa72ce724adb5f5c
Alexander Giagoulas <alexander@giagoulas.com>
2023-02-05 16:38:05 +0100

    add updated_on fieldg

--- app/internal/models/gallery.py
+++ app/internal/models/gallery.py
@@ -1,6 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime
 
 
 class Gallery(BaseModel):
     title: str
     description: str
+    updated_on: datetime = Field(default_factory=datetime.utcnow)
